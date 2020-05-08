8463b18225d91aebb86809158a6612a32768d18c
sovathana-slash <sovathana@slash.co>
2020-05-08 14:33:42 +0700

    add typescript support

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export { VLCPlayer } from './VLCPlayer';
+export { VlCPlayerView } from './playerView/index';
+export { VlCPlayerViewByMethod } from './playerViewByMethod/index';
+export { VlcSimplePlayer } from './playerViewByMethod/index';
